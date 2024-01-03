61106d148b91dd59dff277ee93e02e59953cee82
VasiliyMac <cartario@yandex.ru>
2024-01-03 12:59:33 +0400

    week1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,25 +4,17 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
+urlpatterns = [   
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-
     # path for login
-
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
