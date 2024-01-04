6980bb023ffde33d52fed134b0cb38b584e6da94
VasiliyMac <cartario@yandex.ru>
2024-01-04 14:18:45 +0400

    week3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.utils.timezone import now
 
@@ -5,10 +6,38 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length = 50)
+    description = models.CharField(null = True, max_length = 200)
+    def __str__(self):
+      return self.name
+
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, null = True, on_delete = models.CASCADE)
+    name = models.CharField(null=False, max_length = 50)   
+    dealer_id = models.IntegerField()
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, "SUV"), (WAGON, "Station wagon")]
+
+    model_type = models.CharField(max_length =100, choices = CAR_CHOICES, default = 'SEDAN')
+
+    YEAR_CHOICES = []
+    for r in range(1969, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r, r))
+
+    year = models.IntegerField(
+        ('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+    def __str__(self):
+        return self.name + ", " + str(self.year) + ", " + self.model_type
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
